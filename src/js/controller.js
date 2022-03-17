6070e6c34a0f7f442e6a6801ac4b855d489a758d
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-03-17 15:09:10 +0500

    Add loading animation

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../img/icons.svg'; // Parcel 2
-console.log(icons);
+import 'core-js/stable'; // polyfilling everything else
+import 'regenerator-runtime/runtime'; // polyfilling async/await
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -16,9 +17,22 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+  <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+  </div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
     // 1. Loading recipe
+    renderSpinner(recipeContainer);
     const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
     const data = await res.json();
 
