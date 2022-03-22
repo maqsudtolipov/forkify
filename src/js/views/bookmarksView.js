cfb465d091e2f6ede2b22027db8685d68c364b85
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-03-22 20:45:46 +0500

    Add bookmarks to bookmarks list

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -3,12 +3,12 @@ import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
-  _parentElement = document.querySelector('.results');
-  _errorMessage = 'No recipes found for your query! Please try again.';
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it ;)';
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(result => previewView.render(result, false)).join('');
+    return this._data.map(bookmark => previewView.render(bookmark, false)).join('');
   }
 }
 
