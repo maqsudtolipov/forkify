0d4877f73f3035037d9823d12c9bbef82d920802
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-03-23 20:33:33 +0500

    Write documentation for render() function

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2685,7 +2685,15 @@ var _iconsSvg = require("url:../../img/icons.svg");
 var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
 class View {
     _data;
-    render(data, render = true) {
+    /**
+   * Render the recieved object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup is returned if render=false
+   * @this {Object} View instance
+   * @author Maqsud Tolipov
+   * @todo Finish implementation
+   */ render(data, render = true) {
         if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
         this._data = data;
         const markup = this._generateMarkup();
